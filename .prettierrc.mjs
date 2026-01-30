7bf25423a56f248ad15faa3c74198ab69c65ae8a
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2026-01-30 18:36:03 +0000

    Refactor code to use single quotes for string literals and improve consistency across components

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,17 +1,17 @@
 /** @type {import("prettier").Config} */
 export default {
   semi: true,
-  singleQuote: false,
+  singleQuote: true,
   tabWidth: 2,
-  trailingComma: "all",
+  trailingComma: 'all',
 
-  plugins: ["prettier-plugin-astro"],
+  plugins: ['prettier-plugin-astro'],
 
   overrides: [
     {
-      files: "*.astro",
+      files: '*.astro',
       options: {
-        parser: "astro",
+        parser: 'astro',
       },
     },
   ],
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://hawksley.dev",
-  base: "/astro-site",
-  trailingSlash: "always",
+  site: 'https://hawksley.dev',
+  base: '/astro-site',
+  trailingSlash: 'always',
 });
